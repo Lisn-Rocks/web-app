89ed02c1e103ede43b73200a0916a401eeebcdbc
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2020-04-30 20:36:46 +0100

    More efficient deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,14 @@
   
 echo $(date)
 
-if ([ "$(git pull)" != "Already up to date." ] || [ ! -d "node_modules" ]); then
-        npm install
+git_pull_response=$(git pull)
+
+# Only run `npm install` when `package-lock.json` file has been updated.
+if git show | grep -Fq "package-lock.json"; then
+	npm install
+fi
+
+# Only run `npm run build` if `git pull` actually pulls something new.
+if [ "$git_pull_response" != "Already up to date." ]; then
         npm run build
 fi
